a95b5924fb868c2f342933d964bbad38ff110351
Natália Parišková <natalia.pariskova@gmail.com>
2019-12-03 11:51:35 +0100

    prerobenie o nas, vytvorenie tabulky vzhlad

--- Salon_zaverecny/Controllers/KadernictvoesController.cs
+++ Salon_zaverecny/Controllers/KadernictvoesController.cs
@@ -49,6 +49,14 @@ namespace Salon_zaverecny.Controllers
             return View();
         }
 
+
+        public IActionResult Vzhlad()
+        {
+            return View();
+        }
+
+
+
         // POST: Kadernictvoes/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -151,3 +159,5 @@ namespace Salon_zaverecny.Controllers
         }
     }
 }
+
+
