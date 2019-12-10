475d7aae91d997f8b0403e8c19c435c32f2bd9ce
Kristina Valentinyova <kristinavalentinyova@gmail.com>
2019-12-10 12:16:07 +0100

    login, authorize

--- Salon_zaverecny/Controllers/KozmetikasController.cs
+++ Salon_zaverecny/Controllers/KozmetikasController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -20,6 +21,7 @@ namespace Salon_zaverecny.Controllers
             _context = context;
         }
 
+        [Authorize]
         // GET: Kozmetikas
         public async Task<IActionResult> Index()
         {
